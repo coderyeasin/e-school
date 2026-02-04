b7adec603d7012ae69c7c799b0440cf2e44898d3
coderyeasin <yeasin3233@gmail.com>
2026-02-04 22:19:54 +0600

    setup

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Container = () => {
+  return (
+    <div>
+      <h3>Container</h3>
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/components/Features/Features.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div>
+      <h3>Features Component</h3>
+    </div>
+  );
+};
+
+export default Features;
