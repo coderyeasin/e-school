bb133be44bf6e41cfa3289f0f43c91f23088a4f9
coderyeasin <yeasin3233@gmail.com>
2026-02-04 22:27:26 +0600

    navbar

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div>
-      <h3>HeroSection</h3>
-    </div>
+    <section className="hero-section">
+      <div>
+        <h1>Learn With fun on any schedule</h1>
+        <p>Your gateway to knowledge and learning.</p>
+        <button className="cta-button">Get Started</button>
+      </div>
+      <div>
+        <img src="/src/assets/hero-image.png" alt="Hero Image" />
+      </div>
+    </section>
   );
 };
 
