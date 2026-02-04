537967e7662b9eba6132fcd3ed61c2f37ce9858a
coderyeasin <yeasin3233@gmail.com>
2026-02-04 22:25:52 +0600

    navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,28 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h3>Header Component</h3>
-    </div>
+    <header>
+      <nav className="flex justify-around items-center">
+        <img src="/src/assets/logo.png" alt="Logo" />
+        <ul>
+          <li>
+            <a href="#books">Books</a>
+          </li>
+          <li>
+            <a href="#courses">Courses</a>
+          </li>
+          <li>
+            <a href="#others">Others</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#signin">Sign in</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
